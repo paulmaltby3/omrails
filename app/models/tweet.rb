b3def861c45c18cebef82449d7a7a6b3db3a263d
Paul Maltby <paul.maltby3@gmail.com>
2019-10-30 07:45:18 +0000

    new way to orient tails image

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,6 +6,6 @@ class Tweet < ApplicationRecord
   
   acts_as_votable
   
-  has_attached_file :image#, source_file_options { all: '-auto-orient' }
+  has_attached_file :image, :convert_options => { :all => '-auto-orient' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
